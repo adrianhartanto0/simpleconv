6a098d2310e25eb54186d0d44946573edd143689
Ericko Hartanto <erickohartanto@erickos-macbook-pro.local>
2016-02-16 01:24:09 +0800

    Initial Commit

---
+++ convert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  convert
+//
+//  Created by Ericko Hartanto on 2/16/16.
+//  Copyright © 2016 adrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
